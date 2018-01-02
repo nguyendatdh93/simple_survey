41fef9ef199b00c9859d75804855804720f1dea1
nguyendat <nguyen.van.dat@alliedtechbase.com>
2018-01-02 10:36:51 +0700

    update core vendor

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Basevendor\AdminTemplate\Providers\AdminLTETemplateServiceProvider::class,
+        Atb\Corevendor\Providers\AdminLTETemplateServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
 
     ],
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'AdminLTE' => Basevendor\AdminTemplate\AdminLTE::class,
+        'AdminLTE' => Atb\Corevendor\AdminLTE::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'FormSimple' => App\BaseWidget\Form::class
