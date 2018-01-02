38f23649adce13162fec7da02bb912dac64756d4
nguyendat <nguyen.van.dat@alliedtechbase.com>
2018-01-02 16:47:48 +0700

    update

--- resources/views/vendor/admin/form.blade.php
+++ resources/views/vendor/admin/form.blade.php
@@ -6,19 +6,6 @@
 
 @section('main-content-form')
     <div class="container-fluid spark-screen">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <h1>
-                    General Form Elements
-                    <small>Preview</small>
-                </h1>
-                <ol class="breadcrumb">
-                    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                    <li><a href="#">Forms</a></li>
-                    <li class="active">General Elements</li>
-                </ol>
-            </section>
-
             <!-- Main content -->
             <section class="content">
                 <div class="row">
