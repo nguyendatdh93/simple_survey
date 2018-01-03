7ce8fc4b96670e1998def34b8517ee4afc8d5b97
nguyendat <nguyen.van.dat@alliedtechbase.com>
2018-01-03 12:03:25 +0700

    update c

--- resources/views/vendor/admin/auth/partials/social_login.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="social-auth-links text-center">
-    <a href="{{ url('/auth/google') }}" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> {{ trans('adminlte_lang::message.signGoogle+') }}</a>
-</div><!-- /.social-auth-links -->
