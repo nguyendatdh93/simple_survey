b261cdf9729f00d300d9c009c4d6d81e04c7eec1
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-01-03 17:18:10 +0700

    login with google account

--- resources/views/admin/auth/partials/social_login.blade.php
+++ resources/views/admin/auth/partials/social_login.blade.php
@@ -1,3 +1,6 @@
 <div class="social-auth-links text-center">
-    <a href="{{ url('/auth/google') }}" class="btn btn-block btn-social btn-google btn-flat">{{ trans('adminlte_lang::message.signGoogle+') }}</a>
+    <a href="{{ url('/auth/google') }}" class="btn btn-block btn-success btn-flat">{{ trans('adminlte_lang::message.signGoogle+') }} <br> {{ Config::get('config.domain')}}</a>
+    @if ($message = Session::get('error'))
+        <p class="text-danger" style="padding-top: 25px">{{ $message }}</p>
+    @endif
 </div><!-- /.social-auth-links -->
