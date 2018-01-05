33fb989cd9d0af2fafc036b3d25dfc41d6ff9201
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-01-05 16:25:37 +0700

    update download list

--- resources/lang/en/survey.php
+++ resources/lang/en/survey.php
@@ -26,4 +26,5 @@ return [
     'survey_list_table_header_column_survey_published_at' => "Published at",
     'survey_list_table_header_column_survey_closed_at' => "Closed at",
     'survey_list_table_header_column_survey_number_answers' => "Number Answers",
+    'go_download_button' => 'Go to download page'
 ];
--- resources/views/admin/layouts/partials/script_datatable_download_list.blade.php
+++ resources/views/admin/layouts/partials/script_datatable_download_list.blade.php
@@ -9,6 +9,7 @@
             'autoWidth'   : true,
             'createdRow' : function( row, data, dataIndex ) {
 //                var DatatableSerbvice = new DatatableService();
+                $(row).children(".tbl-control").html(addControls(row,data));
                 $(row).children(".tbl-status").html(buttonForStatus(data));
             },
             "columnDefs": [
@@ -20,6 +21,25 @@
                 { "targets": 7,"orderable" : false},
             ],
         });
+
+        function addControls(row,data)
+        {
+            var html            = '',
+                url_redirect_detail = '',
+                url_redirect_copy   = '';
+
+            if(data.indexOf("{{ trans('adminlte_lang::survey.draf') }}") >= 0) {
+                url_redirect_detail = "{{ route("draf") }}/"+ data[0];
+            } else if(data.indexOf("{{ trans('adminlte_lang::survey.published') }}") >= 0) {
+                url_redirect_detail = "{{ route("publish") }}/"+ data[0];
+            } else {
+                url_redirect_detail = "{{ route("close") }}/"+ data[0];
+            }
+
+            html += '<a href="'+ url_redirect_detail +'" class="btn btn-default bg-olive" data-toggle="tooltip" title="{{ trans('adminlte_lang::survey.go_download_button') }}"><i class="fa fa-download"></i></a>';
+
+            return html;
+        }
 
         function buttonForStatus(data)
         {
@@ -38,6 +58,8 @@
 
             return html;
         }
+
+
     });
 
 </script>
