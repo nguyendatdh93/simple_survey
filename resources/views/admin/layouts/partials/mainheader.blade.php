5000285fad1a633f9901f0b9d5a0bdc2cdf5b45f
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-01-09 08:58:57 +0700

    update menu logo m

--- resources/views/admin/layouts/partials/mainheader.blade.php
+++ resources/views/admin/layouts/partials/mainheader.blade.php
@@ -4,7 +4,7 @@
     <!-- Logo -->
     <a href="{{ url('/survey/list') }}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>A</b>LT</span>
+        <span class="logo-mini"></span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><b>{{ $logo }}</b> </span>
     </a>
