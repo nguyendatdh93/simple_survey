a8b09723ad41c1f322304fc4ffcc326006ce1fc4
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-01-12 17:31:22 +0700

    update preview link

--- resources/views/admin/survey/form_survey.blade.php
+++ resources/views/admin/survey/form_survey.blade.php
@@ -4,27 +4,27 @@
     @include('admin::survey.partials.htmlheader')
 </head>
 <body id="form">
+@section('body')
+    <div id="pagetop">
+        <div id="layout">
+            <div class="header">
+                <div class="headerWrap1">
+                    @include('admin::survey.partials.script')
 
-<div id="pagetop">
-    <div id="layout">
+                    @include('admin::survey.partials.form_header')
 
-        <div class="header">
-            <div class="headerWrap1">
-                @include('admin::survey.partials.script')
+                    <!-- ▼年齢認証が必要ない場合は、削除してください -->
+                    <!-- ▲年齢認証が必要ない場合は、削除してください -->
 
-                @include('admin::survey.partials.form_header')
+                    <!-- /.headerWrap1 --></div>
+                <!-- /.header --></div>
+            @include('admin::survey.partials.form_content')
 
-                <!-- ▼年齢認証が必要ない場合は、削除してください -->
-                <!-- ▲年齢認証が必要ない場合は、削除してください -->
+            @include('admin::survey.partials.footer')
 
-                <!-- /.headerWrap1 --></div>
-            <!-- /.header --></div>
-        @include('admin::survey.partials.form_content')
-
-        @include('admin::survey.partials.footer')
-
-        <!-- /.layout --></div>
-    <!-- /.pagetop --></div>
+            <!-- /.layout --></div>
+        <!-- /.pagetop --></div>
+@show
 
 </body>
 </html>
