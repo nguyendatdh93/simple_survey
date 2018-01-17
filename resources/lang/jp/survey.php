e779537506de0e9334a0675cc10d43ff5ca82e9d
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-01-17 13:20:23 +0700

    update dev

--- resources/lang/jp/survey.php
+++ resources/lang/jp/survey.php
@@ -85,4 +85,7 @@ return [
 	'message_confirm_condition' => '規約に承諾してください',
 	'alert_success_answer_survey' => 'Thank you for taking the survey',
 	'column_csv_created_at' => 'Time answered',
+	
+	'radio_label_choice_survey_draft_status' => '下書き',
+	'radio_label_choice_survey_publish_status' => '公開する',
 ];
