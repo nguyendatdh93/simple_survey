cdfb20ae3989d1bd8c6a5b02a9d6a72d0766ca82
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-01-18 13:41:50 +0700

    update language j

--- resources/views/admin/layouts/partials/alert_message.blade.php
+++ resources/views/admin/layouts/partials/alert_message.blade.php
@@ -2,14 +2,12 @@
     @if($message = Session::get('alert_error'))
         <div class="alert alert-danger alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h4><i class="icon fa fa-ban"></i> Alert!</h4>
             {{ $message }}
         </div>
     @endif
     @if($message = Session::get('alert_success'))
         <div class="alert alert-success alert-dismissible">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h4><i class="icon fa fa-check"></i> Alert!</h4>
             {{ $message }}
         </div>
     @endif
