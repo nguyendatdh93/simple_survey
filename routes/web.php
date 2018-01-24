c3c8ee6537d3ad2f073b9b4da72ddd2f6c990b0b
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-01-24 14:39:25 +0700

    update router

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/login', 'Auth\LoginController@showLoginForm');
 
 Route::prefix('auth')->group(function () {
     Route::get('/google', array('as' => 'auth.google', 'uses' => 'Auth\LoginController@loginWithGoogle'));
@@ -50,6 +51,11 @@ Route::get('/image/{image_path?}/{image_name?}', 'HomeController@showImage')->na
 
 Route::prefix('/')->group(function () {
 	Route::get('/thank', 'AnswerSurveyController@showThankPage')->name(\App\Survey::NAME_URL_THANK_PAGE);
+	
+	Route::get('/', function (){
+		return view('admin::errors.404');
+	});
+	
 	Route::get('/{encrypt?}', 'AnswerSurveyController@showQuestionSurvey')->name(\App\Survey::NAME_URL_ANSWER_SURVEY);
 	Route::get('/{encrypt?}/confirm', 'AnswerSurveyController@showFormConfirmAnswerSurvey')->name(\App\Survey::NAME_URL_ANSWER_CONFIRM);
 	Route::get('/{encrypt?}/answer', 'AnswerSurveyController@answerSurvey')->name(\App\Survey::NAME_URL_SUBMIT_CONFIRM);
