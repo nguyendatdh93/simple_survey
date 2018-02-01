d7afee7a87f7b16b6d00405d5522966c6dd6d8db
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-02-01 13:35:17 +0700

    update logic encrypt url

--- app/Http/Services/EncryptionService.php
+++ app/Http/Services/EncryptionService.php
@@ -4,16 +4,28 @@ namespace App\Http\Services;
 use App\Http\Controllers\Controller;
 use Config;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
 
 class EncryptionService extends Controller
 {
-    /**
+	private $key;
+	
+	public function __construct()
+	{
+		$this->key = "MTVzaW1wbGVfc3VydmV5X2tleV9zY3JldGU";
+	}
+	
+	/**
      * @param $plaintext
      * @return string
      */
     public function encrypt($plaintext)
     {
-        return Crypt::encrypt($plaintext);
+	    $encryption_key = base64_decode($this->key);
+	    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+	    $encrypted = openssl_encrypt($plaintext, 'aes-256-cbc', $encryption_key, 0, $iv);
+	    
+	    return base64_encode($encrypted . '::' . $iv);
     }
 
     /**
@@ -22,6 +34,9 @@ class EncryptionService extends Controller
      */
     public function decrypt($ciphertext)
     {
-        return Crypt::decrypt($ciphertext);
+	    $encryption_key = base64_decode($this->key);
+	    list($encrypted_data, $iv) = explode('::', base64_decode($ciphertext), 2);
+	    
+	    return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
     }
 }
--- resources/views/admin/survey/partials/form_content.blade.php
+++ resources/views/admin/survey/partials/form_content.blade.php
@@ -1,7 +1,8 @@
 @section("form-content")
     <div class="formArea">
-        <form method="GET" action="{{ route(\App\Survey::NAME_URL_ANSWER_CONFIRM, ['encrypt' => isset($survey['encrypt_url']) ? $survey['encrypt_url'] : ""]) }}" name="form_answer_survey" id="form-answer-survey">
+        <form method="POST" action="{{ route(\App\Survey::NAME_URL_ANSWER_CONFIRM, ['encrypt' => isset($survey['encrypt_url']) ? $survey['encrypt_url'] : ""]) }}" name="form_answer_survey" id="form-answer-survey" enctype="multipart/form-data">
             <!-- ▼変更可能エリア※table（formTable）は固定なので変更しないでください -->
+            {{ csrf_field() }}
             <table class="formTable">
                 @if(isset($survey['questions'][\App\Question::CATEGORY_CONTENT]))
                     {!! \App\BaseWidget\Survey::formAnswerPattern($survey['questions'][\App\Question::CATEGORY_CONTENT]) !!}
