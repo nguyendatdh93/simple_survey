5459707a5da244a8653abc692d51a64e41edd38b
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-02-01 09:23:49 +0700

    update validate

--- resources/views/admin/survey/form_answer_pattern.blade.php
+++ resources/views/admin/survey/form_answer_pattern.blade.php
@@ -4,9 +4,9 @@
             @if( isset($survey_content['answer']))
                 @php $value = $survey_content['answer']; @endphp
             @endif
-            {!! \App\BaseWidget\Survey::singleText($survey_content, array("name" => "num", "value" => (isset($value) ? $value : ""), "class" => "ipt01","maxlength" => "255")) !!}
+            {!! \App\BaseWidget\Survey::singleText($survey_content, array("name" => "num", "value" => (isset($value) ? $value : ""), "class" => "ipt01","maxlength" => "")) !!}
         @elseif($survey_content['type'] == \App\Question::TYPE_MULTI_TEXT)
-            {!! \App\BaseWidget\Survey::multiText($survey_content, array("id" => "comment", "class" => "txt01","value" => (isset($value) ? $value : "") , "name" => "comment", "maxlength" => "5000", "placeholder" => "コメントをご記入下さい")) !!}
+            {!! \App\BaseWidget\Survey::multiText($survey_content, array("id" => "comment", "class" => "txt01","value" => (isset($value) ? $value : "") , "name" => "comment", "maxlength" => "", "placeholder" => "コメントをご記入下さい")) !!}
         @elseif($survey_content['type'] == \App\Question::TYPE_SINGLE_CHOICE)
             {!! \App\BaseWidget\Survey::singleChoice($survey_content, $survey_content['question_choices'], array("name" => "optradio")) !!}
         @elseif($survey_content['type'] == \App\Question::TYPE_MULTI_CHOICE)
