484b42204eab0723872268e1d6ab1c51ba6cab85
nguyen.van.dat@alliedtechbase.com <nguyen.van.dat@alliedtechbase.com>
2018-02-01 14:23:31 +0700

    update logic of encrypt url

--- app/Http/Services/EncryptionService.php
+++ app/Http/Services/EncryptionService.php
@@ -3,8 +3,6 @@ namespace App\Http\Services;
 
 use App\Http\Controllers\Controller;
 use Config;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Support\Facades\Hash;
 
 class EncryptionService extends Controller
 {
@@ -12,7 +10,7 @@ class EncryptionService extends Controller
 	
 	public function __construct()
 	{
-		$this->key = "MTVzaW1wbGVfc3VydmV5X2tleV9zY3JldGU";
+		$this->key = Config::get('config.key_secret_encrypt_url');
 	}
 	
 	/**
@@ -22,10 +20,12 @@ class EncryptionService extends Controller
     public function encrypt($plaintext)
     {
 	    $encryption_key = base64_decode($this->key);
-	    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+	    
+	    $iv = substr(hash('sha256', $this->key), 0, 16);
+	    
 	    $encrypted = openssl_encrypt($plaintext, 'aes-256-cbc', $encryption_key, 0, $iv);
 	    
-	    return base64_encode($encrypted . '::' . $iv);
+	    return $this->base32_encode($encrypted);
     }
 
     /**
@@ -35,8 +35,42 @@ class EncryptionService extends Controller
     public function decrypt($ciphertext)
     {
 	    $encryption_key = base64_decode($this->key);
-	    list($encrypted_data, $iv) = explode('::', base64_decode($ciphertext), 2);
+	    $iv = substr(hash('sha256', $this->key), 0, 16);
 	    
-	    return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
+	    return openssl_decrypt($this->base32_decode($ciphertext), 'aes-256-cbc', $encryption_key, 0, $iv);
     }
+	
+	/**
+	 * @param $d
+	 * @return mixed|string
+	 */
+	public function base32_decode($d)
+	{
+		list($t, $b, $r) = array("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567", "", "");
+		
+		foreach(str_split($d) as $c)
+			$b = $b . sprintf("%05b", strpos($t, $c));
+		
+		foreach(str_split($b, 8) as $c)
+			$r = $r . chr(bindec($c));
+		
+		return($r);
+	}
+	
+	/**
+	 * @param $d
+	 * @return mixed|string
+	 */
+	public function base32_encode($d)
+	{
+		list($t, $b, $r) = array("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567", "", "");
+		
+		foreach(str_split($d) as $c)
+			$b = $b . sprintf("%08b", ord($c));
+		
+		foreach(str_split($b, 5) as $c)
+			$r = $r . $t[bindec($c)];
+		
+		return($r);
+	}
 }
