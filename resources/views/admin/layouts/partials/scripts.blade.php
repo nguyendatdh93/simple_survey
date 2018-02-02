1edeb8d8c68411ab8df8eabd125f2772904d8c27
atb_nguyentienphuoc <nguyen.tien.phuoc@alliedtechbase.com>
2018-02-02 11:32:03 +0700

    fix load all datatable scripts

--- resources/views/admin/layouts/partials/scripts.blade.php
+++ resources/views/admin/layouts/partials/scripts.blade.php
@@ -36,9 +36,4 @@
             CKEDITOR.replace(id_ckeditor)
         }
     });
-
 </script>
-
-@include('admin::layouts.partials.script_datatable_survey_list')
-@include('admin::layouts.partials.script_datatable_download_list')
-@include('admin::layouts.partials.script_datatable_download_page')
