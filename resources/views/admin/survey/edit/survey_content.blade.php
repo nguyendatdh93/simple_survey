6a39eb56b69b7e39d89aac744152690eadb5c3e6
atb_nguyentienphuoc <nguyen.tien.phuoc@alliedtechbase.com>
2018-02-05 17:41:27 +0700

    fix validate confirmation

--- resources/views/admin/survey/edit/survey_content.blade.php
+++ resources/views/admin/survey/edit/survey_content.blade.php
@@ -82,7 +82,7 @@
 												<div class="col-md-12">
 													<textarea id="question_{{ $question_number }}_confirmation_text"
 															  name="question_{{ $question_number }}_confirmation_text"
-															  class="form-control jsQuestionConfirmationText required jsInputLimit5000"
+															  class="form-control jsQuestionConfirmationText required"
 															  rows="10">{{ empty($question['confirm_text']) ? '' : $question['confirm_text'] }}</textarea>
 													<p class="help-block">{{ trans('survey.confirmation_help_block') }}</p>
 													<p class="jsError" style="color: red; display: none;"></p>
