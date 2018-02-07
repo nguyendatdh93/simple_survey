2479896bb643e7e9136dccca516ff6228fb0b859
atb_nguyentienphuoc <nguyen.tien.phuoc@alliedtechbase.com>
2018-02-07 17:40:51 +0700

    prevent back action at thank page

--- resources/views/user/survey/answer/thank.blade.php
+++ resources/views/user/survey/answer/thank.blade.php
@@ -16,3 +16,40 @@
         <p>送信完了いたしました。<br>このたびはキャンペーンにご参加いただき、<br>誠にありがとうございました。</p>
         <!-- /.secThanks1 --></div>
 @endsection
+
+@section('addition-script')
+<script>
+    (function (global) {
+        if(typeof (global) === "undefined") {
+            console.log("window is undefined");
+            return;
+        }
+
+        var _hash = "!";
+        var noBackPlease = function () {
+            global.location.href += "#";
+            global.setTimeout(function () {
+                global.location.href += "!";
+            }, 50);
+        };
+
+        global.onhashchange = function () {
+            if (global.location.hash !== _hash) {
+                global.location.hash = _hash;
+            }
+        };
+
+        global.onload = function () {
+            noBackPlease();
+            document.body.onkeydown = function (e) {
+                var elm = e.target.nodeName.toLowerCase();
+                if (e.which === 8 && (elm !== 'input' && elm  !== 'textarea')) {
+                    e.preventDefault();
+                }
+                e.stopPropagation();
+            };
+        }
+
+    })(window);
+</script>
+@stop
